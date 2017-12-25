88fa0a88c1b9b5a49fdc11f393f00d0d0a850c72
Siriwat Suananpornpanit <s2win@live.com>
2017-12-25 14:25:17 +0700

    update partial

---
+++ MCVSPA/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCVSPA.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView("_SignOn");
+        }
+
+        [ActionName("SignOn")]
+        public IActionResult SignIn(MCVSPA.Models.SignOn obj)
+        {
+            
+            return PartialView("Home/_SignOn");
+        }
+    }
+}
---
+++ MCVSPA/Models/SignOn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCVSPA.Models
+{
+    public class SignOn
+    {
+        public string UserOrEmail { get; set; }
+        public string Sword { get; set; }
+    }
+}
